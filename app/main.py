6822539c68eb99976fcc2f050510580baef61280
Bansi55-notFound <bansisapariya5@gmail.com>
2025-12-30 18:51:33 +0530

    Load ML pipeline at FastAPI startup

--- app/main.py
+++ app/main.py
@@ -1,5 +1,31 @@
+import os
+
+import joblib
 from fastapi import FastAPI
+from app.schema import CancerInput
+import pandas as pd
+
 app = FastAPI(title="Breast Cancer Prediction API")
+
+MODEL_PATH = os.path.join("models", "cancer_pipeline.joblib")
+
+model=None
+
+
+"""@app.on_event("startup")
+
+Runs once when API starts
+Model is loaded one time
+Stored in memory (model)
+Every request can reuse it
+
+📌 This avoids:
+Loading model on every API call (very bad practice)"""
+@app.on_event("startup")
+def load_model():
+    global model
+    model = joblib.load(MODEL_PATH)
+    print("✅ ML model loaded successfully")
 
 @app.get("/")
 def health_check():
